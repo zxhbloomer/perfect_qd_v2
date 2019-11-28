803e88d377873584bcbcc6c360399402417e84c1
zxhbloomer <zxhbloomer@gmail.com>
2019-11-28 22:31:26 +0800

    。。

--- src/permission.js
+++ src/permission.js
@@ -37,13 +37,13 @@ router.beforeEach(async(to, from, next) => {
         try {
           // get user info
           // note: roles must be a object array! such as: ['admin'] or ,['developer','editor']
-          const { roles } = await store.dispatch('user/getUserInfoAction')
+          // const { roles } = await store.dispatch('user/getUserInfoAction')
 
           // generate accessible routes map based on roles
-          const accessRoutes = await store.dispatch('permission/generateRoutes', roles)
+          // const accessRoutes = await store.dispatch('permission/generateRoutes', roles)
           // 动态添加路由
-          debugger
-          router.addRoutes(accessRoutes)
+          console.log(router)
+          // router.addRoutes(accessRoutes)
 
           // hack method to ensure that addRoutes is complete
           // set the replace: true, so the navigation will not leave a history record
