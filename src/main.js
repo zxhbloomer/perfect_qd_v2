6f323130bdf1495a83a3cdd2a601eb6f98f5c063
zxhbloomer@gmail.com <Lyyzxh030324>
2020-04-21 17:16:51 +0800

    ..

--- src/main.js
+++ src/main.js
@@ -19,6 +19,8 @@ import './utils/error-log' // error log
 
 import * as filters from './filters' // global filters
 
+import '@babel/polyfill'
+
 /**
  * If you don't want to use mock-server
  * you want to use MockJs for mock api
