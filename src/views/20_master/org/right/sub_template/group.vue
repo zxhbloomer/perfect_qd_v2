1b946c023dd0c9f4a12697dd183a9fbeaa8bb533
zxhbloomer <zxhbloomer@gmail.com>
2020-05-04 22:50:34 +0800

    ..

--- src/views/20_master/org/right/sub_template/group.vue
+++ src/views/20_master/org/right/sub_template/group.vue
@@ -50,7 +50,8 @@
           </el-tooltip>
         </template>
       </el-table-column>
-      <el-table-column sortable="custom" min-width="150" :sort-orders="settings.sortOrders" prop="u_time" label="更新时间" />
+      <el-table-column sortable="custom" min-width="100" :sort-orders="settings.sortOrders" prop="u_name" label="更新人" />
+      <el-table-column sortable="custom" min-width="180" :sort-orders="settings.sortOrders" prop="u_time" label="更新时间" />
     </el-table>
     <pagination ref="minusPaging" :total="dataJson.paging.total" :page.sync="dataJson.paging.current" :limit.sync="dataJson.paging.size" @pagination="getDataList" />
 
